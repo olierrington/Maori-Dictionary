00ef900c9920bf4b35a8bb13653ad7e681c7acf6
Oliver Errington <olierrington@icloud.com>
2021-06-19 14:42:16 +1200

    Account page

--- app.py
+++ app.py
@@ -82,6 +82,14 @@ def render_search():
             flash('Search query cannot be longer than 20 characters')
             return redirect(request.referrer)
 
+        elif len(search_query) == 0:
+            flash('Please enter a search query')
+            return redirect(request.referrer)
+
+        elif ' ' in search_query:
+            flash('Search query can not contain a space')
+            return redirect(request.referrer)
+
         else:  # if search query clean then search it
             return redirect('/searchresults/{}'.format(search_query))
 
@@ -91,8 +99,7 @@ def render_search():
 @app.route('/searchresults/<search_query>', methods=["GET", "POST"])
 # search results
 def render_searchresults(search_query):
-
-    search_filter = 'maori_filter' # set default filter as maori filter
+    search_filter = 'maori_filter'  # set default filter as maori filter
 
     if request.method == "POST":
         # get data from filter form
@@ -122,14 +129,19 @@ def render_searchresults(search_query):
             OR definition LIKE '%{0}%'
             OR word_level LIKE '%{0}%'
             ORDER BY word_id DESC""".format(search_query)
-    else:
+    elif search_filter == 'maori_filter':
         query = """SELECT word_id, maori, english, word_level FROM words 
             WHERE maori LIKE '%{0}%' 
             OR english LIKE '%{0}%' 
             OR definition LIKE '%{0}%'
             OR word_level LIKE '%{0}%'
             ORDER BY maori, english ASC""".format(search_query)
+    else:
+        query = """SELECT word_id, maori, english, word_level FROM words
+        ORDER BY maori, english ASC"""
     cur = con.cursor()
+
+    print('hi')
 
     try:
         cur.execute(query)  # executes the query into db
@@ -205,7 +217,6 @@ def render_addword(cat_id):
         english = request.form['english'].strip().lower()
         definition = request.form['definition'].strip()
         word_level = request.form['word_level']
-        date_added = request.form['date_added']
 
         # data validation
         if len(maori) > 20:
@@ -214,22 +225,19 @@ def render_addword(cat_id):
         elif len(english) > 20:
             flash('English word cannot be longer than 20 characters')
             return redirect(request.referrer)
-        elif len(definition) < 5:
-            flash('Definition cannot be less than 5 characters')
-            return redirect(request.referrer)
-        elif len(date_added) > 10:
-            flash('Date cannot be longer than 10 characters')
+        elif not 5 < len(definition) < 100:
+            flash('Definition must be between 5 and 100 characters')
             return redirect(request.referrer)
         else:
             # if data is good then connect to db and upload it
             con = create_connection(DB_NAME)
             query = """INSERT INTO words
                     (word_id, maori, english, cat_id, definition, word_level, user_id, image, date_added)
-                    VALUES(NULL, ?, ?, ?, ?, ?, ?, 'noimage.png', ?)"""
+                    VALUES(NULL, ?, ?, ?, ?, ?, ?, 'noimage.png', strftime('%d-%m-%Y','now'))"""
             cur = con.cursor()
 
             try:  # try to execute query
-                cur.execute(query, (maori, english, cat_id, definition, word_level, session['user_id'], date_added))
+                cur.execute(query, (maori, english, cat_id, definition, word_level, session['user_id']))
 
             except sqlite3.Error:  # catch unexpected errors
                 flash('Unknown Error')
@@ -238,14 +246,14 @@ def render_addword(cat_id):
             con.commit()
             con.close()
             flash('Successfully added word')
-            return redirect(f'/category/{cat_id}')
+            return redirect('/category/{}'.format(cat_id))
 
     con = create_connection(DB_NAME)
     # connect to the database to select the selected category
     query = """SELECT cat_id, category 
             FROM categories WHERE cat_id=?"""
     cur = con.cursor()
-    cur.execute(query, (cat_id,))  # execute query
+    cur.execute(query, (cat_id, ))  # execute query
     cat_name_list = cur.fetchall()  # puts results into list
 
     if len(cat_name_list) == 0:
@@ -409,13 +417,13 @@ def render_editword_page(word_id):
         else:
             con = create_connection(DB_NAME)
             query = """UPDATE words 
-                    SET maori = ?, english = ?, cat_id = ?, 
-                    definition = ?, word_level = ?, image = ? 
+                    SET maori = ?, english = ?, cat_id = ?, definition = ?, 
+                    word_level = ?, user_id=?, image = ?, date_added = strftime('%d-%m-%Y','now') 
                     WHERE word_id=?"""
             cur = con.cursor()
 
             try:  # try to execute query
-                cur.execute(query, (maori, english, cat_id, definition, word_level, image, word_id))
+                cur.execute(query, (maori, english, cat_id, definition, word_level, session['user_id'], image, word_id))
 
             except sqlite3.Error:  # catch unexpected errors
                 flash('Unknown error')
@@ -626,10 +634,211 @@ def is_logged_in():
         return True
 
 
+@app.route('/user')
+# user account
+def render_user_page():
+    if not is_logged_in():  # redirects home if not logged in
+        flash('You are not logged in')
+        return redirect('/')
+
+    con = create_connection(DB_NAME)
+
+    # selects user info
+    query = """SELECT user_id, fname, lname, email
+    FROM users
+    WHERE user_id=?"""
+    cur = con.cursor()
+    cur.execute(query, (session['user_id'],))
+    user_info = cur.fetchall()
+
+    # selects words that have been added by the user
+    query = """SELECT word_id, maori, english, user_id, date_added
+    FROM words
+    WHERE user_id=?
+    ORDER BY date_added ASC"""
+    cur = con.cursor()
+    cur.execute(query, (session['user_id'],))
+    user_words_info = cur.fetchall()
+
+    con.close()
+
+    return render_template('user.html', categories=get_categories(),
+                           logged_in=is_logged_in(), user_info=user_info,
+                           user_words_info=user_words_info)
+
+
+@app.route('/publicuser/<user_id>')
+# user public account
+def render_userpublic_page(user_id):
+
+    con = create_connection(DB_NAME)
+
+    # selects user info
+    query = """SELECT user_id, fname, lname, email
+    FROM users
+    WHERE user_id=?"""
+    cur = con.cursor()
+    cur.execute(query, (user_id,))
+    user_info = cur.fetchall()
+
+    # selects words that have been added by the user
+    query = """SELECT word_id, maori, english, user_id, date_added
+    FROM words
+    WHERE user_id=?
+    ORDER BY date_added ASC"""
+    cur = con.cursor()
+    cur.execute(query, (user_id,))
+    user_words_info = cur.fetchall()
+
+    con.close()
+
+    if len(user_info) == 0:
+        flash('User does not exist')
+        return redirect('/')
+
+    return render_template('publicuser.html', categories=get_categories(),
+                           logged_in=is_logged_in(), user_info=user_info,
+                           user_words_info=user_words_info)
+
+
+@app.route('/usersettings', methods=['GET', 'POST'])
+# user settings
+def render_usersettings_page():
+    if not is_logged_in():  # if not logged in redirect
+        flash('You are not logged in')
+        return redirect('/')
+
+    con = create_connection(DB_NAME)
+    # get user info
+    query = """SELECT user_id, fname, lname, email, password
+        FROM users
+        WHERE user_id=?"""
+    cur = con.cursor()
+    cur.execute(query, (session['user_id'],))
+    user_info = cur.fetchall()
+
+    con.close()
+
+    if request.method == 'POST':
+        print(request.form)
+        # get info from form and clean it
+        fname = request.form.get('fname').strip().title()
+        lname = request.form.get('lname').strip().title()
+        email = request.form.get('email').strip().lower()
+        password = request.form.get('password')
+        db_password = user_info[0][4]
+
+        # data validation
+        if len(fname) > 20:
+            flash('First name cannot be greater than 20 characters')
+            return redirect(request.referrer)
+
+        elif len(lname) > 30:
+            flash('Last name cannot be greater than 30 characters')
+            return redirect(request.referrer)
+
+        elif len(email) > 50:
+            flash('Email cannot be greater than 50 characters')
+            return redirect(request.referrer)
+
+        # check if password is correct
+        elif not bcrypt.check_password_hash(db_password, password):
+            flash('Password incorrect')
+            return redirect(request.referrer)
+
+        else:  # if good then update
+            con = create_connection(DB_NAME)
+            query = """UPDATE users 
+            SET fname = ?, lname = ?, email = ? 
+            WHERE user_id=?"""
+            cur = con.cursor()
+
+            try:  # try to execute query
+                cur.execute(query, (fname, lname, email, user_info[0][0]))
+
+            except sqlite3.Error:  # catch unexpected errors
+                flash('Unknown error')
+                return redirect(request.referrer)
+
+            con.commit()
+            con.close()
+            flash('Successfully edited details')
+            return redirect('/user')
+
+    return render_template('usersettings.html', categories=get_categories(),
+                           logged_in=is_logged_in(), user_info=user_info)
+
+
+@app.route('/changepassword', methods=['GET', 'POST'])
+# password change
+def render_changepassword_page():
+    # if isnt logged in then redirect user
+    if not is_logged_in():
+        flash('You are not logged in')
+        return redirect('/')
+
+    con = create_connection(DB_NAME)
+    # get user details
+    query = """SELECT user_id, fname, lname, email, password
+        FROM users
+        WHERE user_id=?"""
+    cur = con.cursor()
+    cur.execute(query, (session['user_id'],))
+    user_info = cur.fetchall()
+
+    con.close()
+
+    if request.method == 'POST':
+        print(request.form)
+        # get info from form and clean it
+        current_password = request.form.get('current_password')
+        new_password = request.form.get('new_password')
+        con_new_password = request.form.get('con_new_password')
+        db_password = user_info[0][4]
+
+        # data validation
+        if new_password != con_new_password:
+            flash('New passwords do not match')
+            return redirect(request.referrer)
+
+        elif len(new_password) > 20:
+            flash('Password cannot be greater than 30 characters')
+            return redirect(request.referrer)
+
+        # check if password is correct
+        elif not bcrypt.check_password_hash(db_password, current_password):
+            flash('Current password is incorrect')
+            return redirect(request.referrer)
+
+        else:
+            hashed_password = bcrypt.generate_password_hash(new_password)  # hash the password
+            # if everything good then change password
+            con = create_connection(DB_NAME)
+            query = """UPDATE users 
+            SET password = ?
+            WHERE user_id=?"""
+            cur = con.cursor()
+
+            try:  # try to execute query
+                cur.execute(query, (hashed_password, user_info[0][0]))
+
+            except sqlite3.Error:  # catch unexpected errors
+                flash('Unknown error')
+                return redirect(request.referrer)
+
+            con.commit()
+            con.close()
+            flash('Successfully updated password')
+            return redirect('/user')
+
+    return render_template('changepassword.html', categories=get_categories(),
+                           logged_in=is_logged_in(), user_info=user_info)
+
+
 @app.errorhandler(404)
 # 404 page
 def page_not_found(e):
-    return render_template('404.html', categories=get_categories())
+    return render_template('404.html', categories=get_categories(), logged_in=is_logged_in())
 
 
 if __name__ == '__main__':
